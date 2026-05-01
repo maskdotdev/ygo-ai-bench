a9dbddade4bde9746c7b2d371680968365ee0b8b
mask <maskdotdev@gmail.com>
2026-05-01 08:03:42 -0500

    Implement Lua pendulum summon restriction helper

--- src/engine/lua/card-field-names.ts
+++ src/engine/lua/card-field-names.ts
@@ -4,6 +4,7 @@ export const cardFieldNames = [
   "AddMustBeSpecialSummoned",
   "AddMustBeFusionSummoned",
   "AddMustBeLinkSummoned",
+  "AddMustBePendulumSummoned",
   "AddCannotBeNormalSummoned",
   "AddCannotBeFlipSummoned",
   "EnableReviveLimit",
