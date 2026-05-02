77b7e838d965449d9d9180d22b62f7b5356eed2e
mask <maskdotdev@gmail.com>
2026-05-02 02:03:43 -0500

    Extract Lua card type predicates

--- src/engine/lua/card-api.ts
+++ src/engine/lua/card-api.ts
@@ -23,6 +23,7 @@ import { installCardStatusApi } from "#lua/card-status-api.js";
 import { installCardSummonApi } from "#lua/card-summon-api.js";
 import { installCardSummonPredicateApi } from "#lua/card-summon-predicate-api.js";
 import { installCardTableApi, pushCardTable } from "#lua/card-table-api.js";
+import { installCardTypePredicateApi } from "#lua/card-type-predicate-api.js";
 import { linkedGroupUidsForCard, linkedZoneMask } from "#lua/duel-api/location.js";
 import { pushGroupTable } from "#lua/group-api.js";
 import {
@@ -123,48 +124,7 @@ function installStateHelpers<EffectRecord extends LuaCardApiEffectRecord>(L: unk
   pushNumberGetter(L, "GetAttackAnnouncedCount", session, (card) => (card ? session.state.attacksDeclared.filter((uid) => uid === card.uid).length : 0));
   pushBooleanGetter(L, "CanGetPiercingRush", session, (card) => Boolean(card && canGetPiercingRush(session.state, card, hostState)));
   installCardRushApi(L, session, hostState);
-  pushBooleanGetter(L, "IsMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0));
-  pushBooleanGetter(L, "IsMonsterCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0));
-  pushBooleanGetter(L, "IsNotTuner", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1000) === 0));
-  pushBooleanGetter(L, "IsSpell", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2) !== 0));
-  pushBooleanGetter(L, "IsSpellCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2) !== 0));
-  pushBooleanGetter(L, "IsTrap", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x4) !== 0));
-  pushBooleanGetter(L, "IsSpellTrap", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x6) !== 0));
-  pushBooleanGetter(L, "IsSpellTrapCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x6) !== 0));
-  pushBooleanGetter(L, "IsSpirit", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x200000) !== 0));
-  pushBooleanGetter(L, "IsActionCard", session, (card) => (cardTypeFlags(card) & 0x10000000) !== 0 && (cardTypeFlags(card) & 0x80000) === 0);
-  pushBooleanGetter(L, "IsActionSpell", session, (card) => (cardTypeFlags(card) & 0x10000002) === 0x10000002 && (cardTypeFlags(card) & 0x80000) === 0);
-  pushBooleanGetter(L, "IsActionTrap", session, (card) => (cardTypeFlags(card) & 0x10000004) === 0x10000004 && (cardTypeFlags(card) & 0x80000) === 0);
-  pushBooleanGetter(L, "IsActionField", session, (card) => (cardTypeFlags(card) & 0x10080000) === 0x10080000);
-  pushBooleanGetter(L, "IsEquipCard", session, (card) => (cardTypeFlags(card) & 0x40000) !== 0);
-  pushBooleanGetter(L, "IsEquipSpell", session, (card) => cardTypeFlags(card) === 0x40002);
-  pushBooleanGetter(L, "IsEquipTrap", session, (card) => (cardTypeFlags(card) & 0x40004) === 0x40004);
-  pushBooleanGetter(L, "IsFieldSpell", session, (card) => (cardTypeFlags(card) & 0x80002) === 0x80002);
-  pushBooleanGetter(L, "IsLinkSpell", session, (card) => cardTypeFlags(card) === 0x4000002);
-  pushBooleanGetter(L, "IsNormalSpell", session, (card) => cardTypeFlags(card) === 0x2);
-  pushBooleanGetter(L, "IsNormalTrap", session, (card) => cardTypeFlags(card) === 0x4);
-  pushBooleanGetter(L, "IsNormalSpellTrap", session, (card) => cardTypeFlags(card) === 0x2 || cardTypeFlags(card) === 0x4);
-  pushBooleanGetter(L, "IsCounterTrap", session, (card) => (cardTypeFlags(card) & 0x100004) === 0x100004);
-  pushBooleanGetter(L, "IsContinuousSpell", session, (card) => (cardTypeFlags(card) & 0x20002) === 0x20002);
-  pushBooleanGetter(L, "IsRitualSpell", session, (card) => (cardTypeFlags(card) & 0x82) === 0x82);
-  pushBooleanGetter(L, "IsContinuousTrap", session, (card) => (cardTypeFlags(card) & 0x20004) === 0x20004);
-  pushBooleanGetter(L, "IsContinuousSpellTrap", session, (card) => (cardTypeFlags(card) & 0x20000) !== 0 && (cardTypeFlags(card) & 0x6) !== 0);
-  pushBooleanGetter(L, "IsFusionMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x41) === 0x41));
-  pushBooleanGetter(L, "IsRitualMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x81) === 0x81));
-  pushBooleanGetter(L, "IsSynchroMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2001) === 0x2001));
-  pushBooleanGetter(L, "IsXyzMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x800001) === 0x800001));
-  pushBooleanGetter(L, "IsPendulumMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1000001) === 0x1000001));
-  pushBooleanGetter(L, "IsEffectMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x21) === 0x21));
-  pushBooleanGetter(L, "IsNonEffectMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0 && (cardTypeFlags(card) & 0x20) === 0));
-  lua.lua_pushcfunction(L, (state: unknown) => {
-    const card = readCard(state, session);
-    const monsterType = lua.lua_isnumber(state, 2) ? lua.lua_tointeger(state, 2) : 0;
-    if (card) card.data.typeFlags = 0x1 | monsterType;
-    return 0;
-  });
-  lua.lua_setfield(L, -2, to_luastring("AddMonsterAttribute"));
-  lua.lua_pushcfunction(L, () => 0);
-  lua.lua_setfield(L, -2, to_luastring("AddMonsterAttributeComplete"));
+  installCardTypePredicateApi(L, session);
   pushBooleanGetter(L, "IsLinked", session, (card) => Boolean(card && isLinkedMonsterZoneCard(session.state, card)));
   pushNumberGetter(L, "GetLinkedZone", session, (card) => (card ? linkedZoneMask(card) : 0));
   lua.lua_pushcfunction(L, (state: unknown) => {
@@ -402,10 +362,6 @@ function canChangeControl(state: DuelState, card: DuelCardInstance, targetPlayer
   return hasZoneSpace(state, targetPlayer, card.location);
 }
 
-function isPendulumCard(card: DuelCardInstance): boolean {
-  return (cardTypeFlags(card) & 0x1000000) !== 0;
-}
-
 function isLinkedMonsterZoneCard(state: DuelState, card: DuelCardInstance): boolean {
   if (card.location !== "monsterZone" || !card.faceUp) return false;
   return monsterZoneCards(state).some((candidate) => {
@@ -499,14 +455,6 @@ function readCard(L: unknown, session: DuelSession | undefined): DuelCardInstanc
   return uid && session ? session.state.cards.find((candidate) => candidate.uid === uid) : undefined;
 }
 
-function readRequestedCodes(L: unknown, startIndex: number): string[] {
-  const codes: string[] = [];
-  for (let index = startIndex; index <= lua.lua_gettop(L); index += 1) {
-    if (lua.lua_isnumber(L, index)) codes.push(String(lua.lua_tointeger(L, index)));
-  }
-  return codes;
-}
-
 function normalizePlayer(value: number): PlayerId {
   return value === 1 ? 1 : 0;
 }
@@ -551,11 +499,6 @@ function locationMaskFromLocation(location: DuelCardInstance["location"] | undef
   if (location === "banished") return 0x20;
   if (location === "extraDeck") return 0x40;
   return 0;
-}
-
-function leaveFieldDestinationMask(card: DuelCardInstance | undefined): number {
-  if (!card || (card.previousLocation !== "monsterZone" && card.previousLocation !== "spellTrapZone")) return 0;
-  return locationMaskFromLocation(card.location);
 }
 
 function positionMaskFromPosition(position: CardPosition | undefined): number {
---
+++ src/engine/lua/card-type-predicate-api.ts
@@ -0,0 +1,64 @@
+import fengari from "fengari";
+import { readCardUid } from "#lua/api-utils.js";
+import { cardTypeFlags } from "#lua/card-stat-api.js";
+import type { DuelCardInstance, DuelSession } from "#duel/types.js";
+
+const { lua, to_luastring } = fengari;
+
+export function installCardTypePredicateApi(L: unknown, session: DuelSession): void {
+  pushBooleanGetter(L, "IsMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0));
+  pushBooleanGetter(L, "IsMonsterCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0));
+  pushBooleanGetter(L, "IsNotTuner", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1000) === 0));
+  pushBooleanGetter(L, "IsSpell", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2) !== 0));
+  pushBooleanGetter(L, "IsSpellCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2) !== 0));
+  pushBooleanGetter(L, "IsTrap", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x4) !== 0));
+  pushBooleanGetter(L, "IsSpellTrap", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x6) !== 0));
+  pushBooleanGetter(L, "IsSpellTrapCard", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x6) !== 0));
+  pushBooleanGetter(L, "IsSpirit", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x200000) !== 0));
+  pushBooleanGetter(L, "IsActionCard", session, (card) => (cardTypeFlags(card) & 0x10000000) !== 0 && (cardTypeFlags(card) & 0x80000) === 0);
+  pushBooleanGetter(L, "IsActionSpell", session, (card) => (cardTypeFlags(card) & 0x10000002) === 0x10000002 && (cardTypeFlags(card) & 0x80000) === 0);
+  pushBooleanGetter(L, "IsActionTrap", session, (card) => (cardTypeFlags(card) & 0x10000004) === 0x10000004 && (cardTypeFlags(card) & 0x80000) === 0);
+  pushBooleanGetter(L, "IsActionField", session, (card) => (cardTypeFlags(card) & 0x10080000) === 0x10080000);
+  pushBooleanGetter(L, "IsEquipCard", session, (card) => (cardTypeFlags(card) & 0x40000) !== 0);
+  pushBooleanGetter(L, "IsEquipSpell", session, (card) => cardTypeFlags(card) === 0x40002);
+  pushBooleanGetter(L, "IsEquipTrap", session, (card) => (cardTypeFlags(card) & 0x40004) === 0x40004);
+  pushBooleanGetter(L, "IsFieldSpell", session, (card) => (cardTypeFlags(card) & 0x80002) === 0x80002);
+  pushBooleanGetter(L, "IsLinkSpell", session, (card) => cardTypeFlags(card) === 0x4000002);
+  pushBooleanGetter(L, "IsNormalSpell", session, (card) => cardTypeFlags(card) === 0x2);
+  pushBooleanGetter(L, "IsNormalTrap", session, (card) => cardTypeFlags(card) === 0x4);
+  pushBooleanGetter(L, "IsNormalSpellTrap", session, (card) => cardTypeFlags(card) === 0x2 || cardTypeFlags(card) === 0x4);
+  pushBooleanGetter(L, "IsCounterTrap", session, (card) => (cardTypeFlags(card) & 0x100004) === 0x100004);
+  pushBooleanGetter(L, "IsContinuousSpell", session, (card) => (cardTypeFlags(card) & 0x20002) === 0x20002);
+  pushBooleanGetter(L, "IsRitualSpell", session, (card) => (cardTypeFlags(card) & 0x82) === 0x82);
+  pushBooleanGetter(L, "IsContinuousTrap", session, (card) => (cardTypeFlags(card) & 0x20004) === 0x20004);
+  pushBooleanGetter(L, "IsContinuousSpellTrap", session, (card) => (cardTypeFlags(card) & 0x20000) !== 0 && (cardTypeFlags(card) & 0x6) !== 0);
+  pushBooleanGetter(L, "IsFusionMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x41) === 0x41));
+  pushBooleanGetter(L, "IsRitualMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x81) === 0x81));
+  pushBooleanGetter(L, "IsSynchroMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x2001) === 0x2001));
+  pushBooleanGetter(L, "IsXyzMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x800001) === 0x800001));
+  pushBooleanGetter(L, "IsPendulumMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1000001) === 0x1000001));
+  pushBooleanGetter(L, "IsEffectMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x21) === 0x21));
+  pushBooleanGetter(L, "IsNonEffectMonster", session, (card) => Boolean(card && (cardTypeFlags(card) & 0x1) !== 0 && (cardTypeFlags(card) & 0x20) === 0));
+  lua.lua_pushcfunction(L, (state: unknown) => {
+    const card = readCard(state, session);
+    const monsterType = lua.lua_isnumber(state, 2) ? lua.lua_tointeger(state, 2) : 0;
+    if (card) card.data.typeFlags = 0x1 | monsterType;
+    return 0;
+  });
+  lua.lua_setfield(L, -2, to_luastring("AddMonsterAttribute"));
+  lua.lua_pushcfunction(L, () => 0);
+  lua.lua_setfield(L, -2, to_luastring("AddMonsterAttributeComplete"));
+}
+
+function pushBooleanGetter(L: unknown, fieldName: string, session: DuelSession, getter: (card: DuelCardInstance | undefined) => boolean): void {
+  lua.lua_pushcfunction(L, (state: unknown) => {
+    lua.lua_pushboolean(state, getter(readCard(state, session)));
+    return 1;
+  });
+  lua.lua_setfield(L, -2, to_luastring(fieldName));
+}
+
+function readCard(L: unknown, session: DuelSession): DuelCardInstance | undefined {
+  const uid = readCardUid(L, 1);
+  return uid ? session.state.cards.find((candidate) => candidate.uid === uid) : undefined;
+}
