3d6503ae997391fedbc37fd91b762d107fa650c0
mask <maskdotdev@gmail.com>
2026-05-17 04:58:21 -0500

    test(engine): ratchet phase-start-standby missed-timing fast resolution

--- test/parity-missed-timing-event-coverage.test.ts
+++ test/parity-missed-timing-event-coverage.test.ts
@@ -27,7 +27,7 @@ const missedTimingChainLifecycleOriginFixtureCount = 12;
 const missedTimingBattleDamageCauseFixtureCount = 4;
 const missedTimingPhaseBoundaryFixtureCount = 22;
 const missedTimingPhaseEndBoundaryCauseFixtureCount = 4;
-const missedTimingPostDeclineOpenFastResolutionFixtureCount = 39;
+const missedTimingPostDeclineOpenFastResolutionFixtureCount = 40;
 const missedTimingEventFamilyCounts = {
   battle: 28,
   chain: 14,
